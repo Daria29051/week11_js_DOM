3f5b65176ca4f9b3f2927e95c67adeca435b3156
unknown <daria.lantyugova@mail.ru>
2023-03-30 13:08:25 +0300

    extra task done

---
+++ extra task/script.js
@@ -0,0 +1,17 @@
+const inputArgument = document.getElementById('input1');
+
+const inputResult = document.getElementById('input2');
+
+const button = document.getElementById('button');
+
+
+const countDoubled = () => {
+    inputResult.value = inputArgument.value ** 2;
+    return inputResult.value;
+};
+
+button.addEventListener('click', countDoubled);
+
+
+
+
